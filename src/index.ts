5ad9635a3b4ba2611c5fda37c1481b901b5b7fa7
aquapi <aquaplmc@gmail.com>
2024-03-10 21:32:39 +0700

    Add cookie export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@
 export * as csp from './modules/csp';
 
 // Cache
-export * as cache from './modules/cache.ts';
+export * as cache from './modules/cache';
+
+// Cookie
+export * as cookie from './modules/cookie';
