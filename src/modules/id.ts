da8c841f93cbc069baf0d71802f31f44ce63c34d
aquapi <aquaplmc@gmail.com>
2024-03-18 10:20:03 +0700

    Optimization: Cache substring

--- src/modules/id.ts
+++ src/modules/id.ts
@@ -11,10 +11,11 @@ export function randomizer(len: number, base: number = 36): () => string {
     const hexLen = hexMap[0].length;
     if (len < hexLen) throw new Error(`Hex map length (${hexLen}) is larger than requested length (${len})`);
 
-    // Reset the store
+    const paddingMod = len % hexLen;
+    const fillPadding = paddingMod === 0 ? '' : `s+=h1[${hexSize}*Math.random()|0];`;
     // eslint-disable-next-line
-    const exceeded = len % hexLen;
+    const paddingCount = ((len / hexLen) | 0) - 1;
 
     // eslint-disable-next-line
-    return Function('h', `'use strict';let s='';let j=1;for(let i=0;i<${((len / hexLen) | 0) - 1};++i)s+=h[${hexSize}*Math.random()|0];${exceeded === 0 ? '' : `s+=h[${hexSize}*Math.random()|0].substring(0,${exceeded});`}return ()=>{if(j===${hexSize}){s='';j=1;for(let i=0;i<${((len / hexLen) | 0) - 1};++i)s+=h[${hexSize}*Math.random()|0];${exceeded === 0 ? '' : `s+=h[${hexSize}*Math.random()|0].substring(0,${exceeded});`}return s+h[0];};return s+h[j++];}`)(hexMap);
+    return Function('h', `'use strict';${paddingMod === 0 ? '' : `const h1=new Array(${hexSize});for(let i=0;i<${hexSize};++i)h1[i]=h[i].substring(0,${paddingMod});`}let s='';let j=1;for(let i=0;i<${paddingCount};++i)s+=h[${hexSize}*Math.random()|0];${fillPadding}return ()=>{if(j===${hexSize}){s='';j=1;for(let i=0;i<${paddingCount};++i)s+=h[${hexSize}*Math.random()|0];${fillPadding}return s+h[0];};return s+h[j++];}`)(hexMap);
 }
