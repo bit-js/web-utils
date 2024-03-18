5da6cecb73e95c82b092c6ccc14a58014ca81f7c
aquapi <aquaplmc@gmail.com>
2024-03-18 22:50:02 +0700

    Enhancement: Create a generator factory

--- src/modules/id.ts
+++ src/modules/id.ts
@@ -1,22 +1,30 @@
 /**
+ * Create a non-secure ID generator factory that uses `Math.random()`
+ */
+export function createBase(radix: number, mapItemLength: number): (length: number) => () => string {
+    const mapSize = radix ** mapItemLength;
+    const randomMap = new Array<string>(mapSize);
+    for (let i = 0; i < mapSize; ++i) randomMap[i] = (i + mapSize).toString(radix).substring(1);
+
+    return (length) => {
+        if (length < mapItemLength) throw new Error(`Output length (${length}) should be larger or equal than ${mapItemLength}`);
+
+        // eslint-disable-next-line
+        const paddingCount = ((length / mapItemLength) >>> 0) - 1;
+        const paddingSize = length % mapItemLength;
+
+        if (paddingSize === 0)
+            return Function('h', `'use strict';let s='';let j=1;for(let i=0;i<${paddingCount};++i)s+=h[${mapSize}*Math.random()>>>0];return ()=>{if(j===${mapSize}){s='';j=1;for(let i=0;i<${paddingCount};++i)s+=h[${mapSize}*Math.random()>>>0];return s+h[0];};return s+h[j++];}`)(randomMap);
+
+        const padding = new Array<string>(mapSize);
+        for (let i = 0; i < mapSize; ++i) padding[i] = randomMap[i].substring(0, paddingSize);
+
+        // eslint-disable-next-line
+        return Function('h', 'h1', `'use strict';let s='';let j=1;for(let i=0;i<${paddingCount};++i)s+=h[${mapSize}*Math.random()>>>0];s+=h1[${mapSize}*Math.random()>>>0];return ()=>{if(j===${mapSize}){s='';j=1;for(let i=0;i<${paddingCount};++i)s+=h[${mapSize}*Math.random()>>>0];s+=h1[${mapSize}*Math.random()>>>0];return s+h[0];};return s+h[j++];}`)(randomMap, padding);
+    };
+}
+
+/**
  * Create a non-secure ID generator that uses `Math.random()`
  */
-export function random(len: number): () => string {
-    if (len < 2) throw new Error(`Cannot create a randomizer with length smaller than 2 (${len})`);
-
-    // The hex map for appending
-    const hexMap = new Array<string>(1296);
-    // Generated string always have length 2
-    for (let i = 0; i < 1296; ++i) hexMap[i] = (i + 1296).toString(36).substring(1);
-
-    const paddingCount = (len >>> 1) - 1;
-    if ((len & 1) === 0)
-        return Function('h', `'use strict';let s='';let j=1;for(let i=0;i<${paddingCount};++i)s+=h[1296*Math.random()>>>0];return ()=>{if(j===1296){s='';j=1;for(let i=0;i<${paddingCount};++i)s+=h[1296*Math.random()>>>0];return s+h[0];};return s+h[j++];}`)(hexMap);
-
-    const paddingHex = new Array<string>(1296);
-    // eslint-disable-next-line
-    for (let i = 0; i < 1296; ++i) paddingHex[i] = hexMap[i][0];
-
-    // eslint-disable-next-line
-    return Function('h', 'h1', `'use strict';let s='';let j=1;for(let i=0;i<${paddingCount};++i)s+=h[1296*Math.random()>>>0];s+=h1[1296*Math.random()|0];return ()=>{if(j===1296){s='';j=1;for(let i=0;i<${paddingCount};++i)s+=h[1296*Math.random()>>>0];s+=h1[1296*Math.random()>>>0];return s+h[0];};return s+h[j++];}`)(hexMap, paddingHex);
-}
+export const random = createBase(16, 2);
